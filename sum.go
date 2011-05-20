db2d0473f48d8043e85c07120ca99462cc85af50
Francisco Souza <francisco@franciscosouza.net>
2011-05-20 16:36:11 -0300

    More on channels

--- sum.go
+++ sum.go
@@ -10,11 +10,23 @@ func Generate(channel chan int) {
 	}
 }
 
+func Filter(inChannel, outChannel chan int, prime int) {
+	for {
+		i := <-inChannel
+		if i % prime != 0 {
+			outChannel <- i
+		}
+	}
+}
+
 func main() {
 	channel := make(chan int)
 	go Generate(channel)
 	for i := 0; i < 100; i++ {
 		number := <-channel
 		fmt.Println(number)
+		ch1 := make(chan int)
+		go Filter(channel, ch1, number)
+		channel = ch1
 	}
 }
